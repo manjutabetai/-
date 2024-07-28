18fd13902cedcf6da299644478e2e90be8d44a06
manjutabetai <39949257+manjutabetai@users.noreply.github.com>
2024-07-29 07:04:00 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [inputText, setInputText] = useState("")
+  const [inputTime, setInputTime] = useState(0)
+  const [records, setRecords] = useState(
+    [ { title: "勉強の記録1", time: 1},
+  { title: "勉強の記録2", time: 3},
+    { title: "勉強の記録3", time: 5}]
+    );
+
+
+  const changeInput = (event)=> setInputText(event.target.value)
+
+  const addItem = ()=>{
+    // const newRecords = {...records}
+  }
+  return (
+    <>
+    
+     <div><p>学習内容</p><input type="text" value={inputText} onChange={changeInput}/></div>
+     <div><p>学習時間</p><input type="number" value={inputText} onChange={changeInput}/><p>時間</p></div>
+
+     <p>学習記録一覧</p>
+     {
+       records.map((record,index)=>{
+         return (
+          <ul>
+            <div>
+              <p>{record.title}　　学習時間は{record.time}時間</p>
+            </div>
+          </ul>
+         )
+       })
+     }
+     <button　onClick={addItem}>登録</button>
+    </>
+  )
+}
+
+export default App
